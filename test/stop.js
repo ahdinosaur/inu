4af6f758abb6f8a475a83679d074fbfdc90b0b95
Piet Geursen <pietgeursen@gmail.com>
2016-05-17 18:08:13 +1200

    Add and pass tests to check streams end on stop()

---
+++ test/stop.js
@@ -0,0 +1,100 @@
+var test = require('tape')
+
+var inu = require('../')
+var pull = inu.pull
+
+test('calling stop() ends actions stream', function (t) {
+  var initialModel = {initial: true}
+  var app = {
+    init: function () {
+      return {model: initialModel}
+    },
+    update: function (model, action) {
+      t.equal(model, initialModel, 'model passed to update is set by initial state')
+      t.end()
+      return {model: model}
+    },
+    view: function (model, dispatch) {
+      return inu.html`<div></div>`
+    }
+  }
+  var streams = inu.start(app)
+  pull(streams.actions(), pull.collect(function (err, actions) {
+    t.error(err)
+    t.equal(actions.length, 0, 'Actions stream ends with no emitted actions')
+    t.end()
+  }))
+  streams.stop()
+})
+
+test('calling stop() ends effects stream', function (t) {
+  var initialModel = {initial: true}
+  var app = {
+    init: function () {
+      return {model: initialModel}
+    },
+    update: function (model, action) {
+      t.equal(model, initialModel, 'model passed to update is set by initial state')
+      t.end()
+      return {model: model}
+    },
+    view: function (model, dispatch) {
+      return inu.html`<div></div>`
+    }
+  }
+  var streams = inu.start(app)
+  pull(streams.effects(), pull.collect(function (err, effects) {
+    t.error(err)
+    t.equal(effects.length, 0, 'Effects stream ends with no emitted actions')
+    t.end()
+  }))
+  streams.stop()
+})
+
+test('calling stop() ends views stream', function (t) {
+  var initialModel = {initial: true}
+  var app = {
+    init: function () {
+      return {model: initialModel}
+    },
+    update: function (model, action) {
+      t.equal(model, initialModel, 'model passed to update is set by initial state')
+      t.end()
+      return {model: model}
+    },
+    view: function (model, dispatch) {
+      return inu.html`<div></div>`
+    }
+  }
+  var streams = inu.start(app)
+  pull(streams.views(), pull.collect(function (err, views) {
+    t.error(err)
+    t.equal(views.length, 0, 'Views stream ends with no emitted actions')
+    t.end()
+  }))
+  streams.stop()
+})
+
+test('calling stop() ends models stream', function (t) {
+  var initialModel = {initial: true}
+  var app = {
+    init: function () {
+      return {model: initialModel}
+    },
+    update: function (model, action) {
+      t.equal(model, initialModel, 'model passed to update is set by initial state')
+      t.end()
+      return {model: model}
+    },
+    view: function (model, dispatch) {
+      return inu.html`<div></div>`
+    }
+  }
+  var streams = inu.start(app)
+  pull(streams.models(), pull.collect(function (err, models) {
+    t.error(err)
+    t.equal(models.length, 0, 'Models stream ends with no emitted actions')
+    t.end()
+  }))
+  streams.stop()
+})
