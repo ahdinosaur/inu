b5094cd8ed330b3674aa3695991eeb8cf83e521f
Piet Geursen <pietgeursen@gmail.com>
2016-05-17 20:13:54 +1200

    Remove skipped tests that shouldn't pass anyway.

--- test/init.js
+++ test/init.js
@@ -22,53 +22,6 @@ test('initial state of model can be set in init', function (t) {
   inu.start(app)
 })
 
-test.skip('returning an action in init dispatches the action to update', function (t) {
-  var expectedAction = {type: 'WEEEEEE'}
-  var app = {
-    init: function () {
-      return {
-        action: expectedAction,
-        model: 0
-      }
-    },
-    update: function (model, action) {
-      t.equal(action, expectedAction)
-      t.end()
-      return {model: model}
-    },
-    view: function (model, dispatch) {
-      dispatch()
-      return inu.html`<div></div>`
-    }
-  }
-  inu.start(app)
-})
-
-// failing test that should pass?
-test.skip('returning an action in init emits the action on the actions stream', function (t) {
-  var expectedAction = {type: 'WEEEEEE'}
-  var app = {
-    init: function () {
-      return {
-        action: expectedAction,
-        model: 0
-      }
-    },
-    update: function (model, action) {
-      return {model: model}
-    },
-    view: function (model, dispatch) {
-      dispatch()
-      return inu.html`<div></div>`
-    }
-  }
-  var streams = inu.start(app)
-  pull(streams.actions(), pull.take(1), pull.drain(function (action) {
-    t.equal(action, expectedAction)
-    t.end()
-  }))
-})
-
 test('returning an effect in init emits the effect on the effects stream', function (t) {
   var expectedEffect = {type: 'WEEEEEE'}
   var app = {
